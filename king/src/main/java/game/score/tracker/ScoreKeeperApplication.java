bbda2015bbf53a40a1f42014bac707eae9d6eefa
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-07-18 23:42:33 +0200

    tests remaining

--- king/src/main/java/game/score/tracker/ScoreKeeperApplication.java
+++ king/src/main/java/game/score/tracker/ScoreKeeperApplication.java
@@ -4,8 +4,9 @@ import com.sun.net.httpserver.HttpServer;
 import game.score.tracker.domain.Score;
 import game.score.tracker.domain.Session;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -32,17 +33,17 @@ public class ScoreKeeperApplication {
                     int userId = Integer.parseInt(requestPath.split("/*/login")[0].split("/")[1]);
                     String sessionId = UUID.randomUUID().toString();
                     sessions.put(sessionId, Session.createNew(sessionId, userId));
-                    exchange.getResponseBody().write(sessionId.getBytes(StandardCharsets.UTF_8));
                     exchange.sendResponseHeaders(200, sessionId.length());
+                    exchange.getResponseBody().write(sessionId.getBytes(StandardCharsets.UTF_8));
 
                 } else if (exchange.getRequestMethod().equals("POST") && requestPath.contains("/score")) {
+                    int levelId = Integer.parseInt(requestPath.split("/*/score")[0].split("/")[1]);
                     Integer userId = Optional.ofNullable(parseQueryParamsToMap(exchange.getRequestURI().getQuery()).get("sessionkey"))
                             .map(sessions::get)
                             .filter(Session::isValid)
                             .map(Session::userId)
                             .orElseThrow(() -> new AccessDeniedException());
-                    int levelId = Integer.parseInt(requestPath.split("/*/score")[0].split("/")[1]);
-                    int scoreValue = new DataInputStream(exchange.getRequestBody()).readInt();
+                    int scoreValue = Integer.parseInt(new BufferedReader(new InputStreamReader(exchange.getRequestBody())).readLine());
                     var levelScoreMap = Optional.ofNullable(scores.get(levelId))
                             .orElseGet(() -> new ConcurrentHashMap<>());
                     levelScoreMap.put(userId, new Score(levelId, userId, scoreValue));
@@ -51,28 +52,33 @@ public class ScoreKeeperApplication {
 
                 } else if (exchange.getRequestMethod().equals("GET") && requestPath.contains("/highscorelist")) {
                     int levelId = Integer.parseInt(requestPath.split("/*/highscorelist")[0].split("/")[1]);//could have used group catching instead
-                    Optional.ofNullable(scores.get(levelId))
+                    String top15Scored = Optional.ofNullable(scores.get(levelId))
                             .stream()
                             .flatMap(levelScoreMap -> levelScoreMap.values().stream())
                             .sorted()
                             .limit(15)
                             .map(Score::toCSV)
                             .collect(Collectors.joining(","));
+                    exchange.sendResponseHeaders(200, top15Scored.length());
+                    exchange.getResponseBody().write(top15Scored.getBytes(StandardCharsets.UTF_8));
                 }
 
             } catch (AccessDeniedException e) {
                 String responseBody = "Authentication failed, error %s".formatted(e.getMessage());
-                exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
                 exchange.sendResponseHeaders(403, responseBody.length());
+                exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
+                e.printStackTrace();
             } catch (Exception e) {
                 String responseBody = "unexpected problem, check the path and parameters for request %s, error %s".formatted(exchange, e.getMessage());
-                exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
                 exchange.sendResponseHeaders(500, responseBody.length());
+                exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
+                e.printStackTrace();
             } finally {
                 exchange.getResponseBody().close();
             }
         });
         httpServer.start();
+        System.out.println(httpServer.getAddress());
     }
 
     public static Map<String, String> parseQueryParamsToMap(String query) {
